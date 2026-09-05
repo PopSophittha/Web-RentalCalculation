2c1358d954c9e8e7fada22329912121f6a56dd31
PopSophittha <sophittha.b@gmail.com>
2026-09-05 12:38:15 +0700

    change house to name

--- receipt.js
+++ receipt.js
@@ -6,7 +6,7 @@ let html = "";
 data.forEach((d, i) => {
   html += `
     <div class="bill">
-      <h3>ห้อง ${d.house}</h3>
+      <h3>ห้อง ${d.name}</h3>
       <p>${d.name}</p>
       <p>รวม: ${d.total} บาท</p>
     </div>
